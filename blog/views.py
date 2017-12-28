093683f1e4586c0471728b4ca2b2cc8de2fef706
alice <liseishestvo@gmail.com>
2017-12-28 13:45:48 +0300

    Теперь посты идут от новейшего к старейшему.

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,9 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now())
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 def post_detail(request, pk):
     Post.objects.get(pk=pk)
 
